494f409cbbb6e33a14c414584dc782ecd9f1e941
IKEDA\duike <eduardoikeda@outlook.com.br>
2024-05-31 16:59:08 -0300

    [ADD] foi adicionada uma valiadação falsa de login para simular o sistema

--- Usuario.java
+++ Usuario.java
@@ -1,3 +1,19 @@
 public class Usuario {
-    // Atributos e métodos relacionados ao usuário
+    private String nome;
+    private String senha;
+
+    public Usuario(String nome, String senha) {
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public boolean ValidarLogin() {
+        // Checando no banco de dados
+        if (this.nome != null && this.senha != null) {
+            System.err.println("Login realizado com sucesso!");
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
